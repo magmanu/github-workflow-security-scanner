8bfd8215e29255d0acd0f181353dedd8b1e563f4
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-04-04 17:33:15 +0100

    Update graphql query

--- query_data.py
+++ query_data.py
@@ -1,10 +1,10 @@
-def return_query(query_type,name,after=None):
+def return_query(query_type,name,branch,after=None):
     if query_type == 'repository':
         owner,name = name.split('/')
         return f"""query {{
                     repository(owner: "{owner}",name: "{name}") {{
                         nameWithOwner
-                        object(expression: "HEAD:.github/workflows/") {{
+                        object(expression: "{branch}:.github/workflows/") {{
                             ... on Tree {{
                                 entries {{
                                     name
@@ -27,7 +27,7 @@ def return_query(query_type,name,after=None):
             edges{{
                 node{{
                 nameWithOwner,
-                object(expression: "HEAD:.github/workflows/") {{
+                object(expression: "{branch}:.github/workflows/") {{
                     ... on Tree {{
                     entries {{
                         name
