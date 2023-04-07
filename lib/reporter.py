e1e7b5bf6148a3bd48723647b79a330300b5b198
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-04-07 08:50:23 +0100

    Output to result.md too

--- lib/reporter.py
+++ lib/reporter.py
@@ -1,50 +1,62 @@
 import subprocess
 import pydash as _
 
-commands = [
-    'echo "| Workflow | Issues | Secrets | Recommendation |" >> $GITHUB_STEP_SUMMARY'
-    'echo "| --- | --- | --- | --- |" >> $GITHUB_STEP_SUMMARY'
-]
+def add_command(string):
+    return f'echo "{string}" >> GITHUB_STEP_SUMMARY'
 
 
 def build_tail_commands(result: dict) -> list[dict]:
-    commands = []
+    strings = []
     for wrkfl_name in result:
-        secrets = (",".join(_.get(result[wrkfl_name], "secrets")))
+        secrets = (",".join(_.get(result[wrkfl_name], "secrets", None)))
 
         issues_list = _.get(result[wrkfl_name],"issues")
         if issues_list:
             for issue in issues_list:
                 desc = _.get(issue, "issue")
                 remediation = _.get(issue, "remediation")
-                string = f'echo "| {wrkfl_name} | {secrets or "None"} | {desc} | {remediation} |" >> $GITHUB_STEP_SUMMARY'
-                commands.append(string)
+                string = f"| {wrkfl_name} | {secrets} | {desc} | {remediation} |"
+                strings.append(string)
         else:
-            string = f'echo "| {wrkfl_name} | {secrets or "None"} | None | None |" >> $GITHUB_STEP_SUMMARY'
-            commands.append(string)
-    return commands
+            string = f"| {wrkfl_name} | {secrets} | None | None |"
+            strings.append(string)
+    return strings
 
 
 def build_commands(tail):
-    commands = [
-        'echo "============>Generating summary"',
-        'echo "| Workflow | Issues | Secrets | Recommendation |" >> $GITHUB_STEP_SUMMARY',
-        'echo "| --- | --- | --- | --- |" >> $GITHUB_STEP_SUMMARY'
+    commands = []
+    header = [
+        "| Workflow | Secrets | Issues | Recommendation |",
+        "| --- | --- | --- | --- |"
     ]
 
-    for command in tail:
-        commands.append(command)
-    print(commands)
+
+    write_to_file(header)
+
+    for string in tail:
+        write_to_file(string)
+        command = add_command(string)
+        commands.append(f"{command}")
     return commands
 
 def report(result:dict):
-    print("===========> REPORT")
+
     tail = build_tail_commands(result)
+
     commands = build_commands(tail)
     for command in commands:
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
+def write_to_file(inputs: str | list):
+    f = open("result.md", "a")
 
+    if isinstance(inputs, list):
+        for element in inputs:
+            f.write(element)
+            f.write(f"\n")
 
-
+    else:
+        f.write(inputs)
+        f.write(f"\n")
+    f.close()
