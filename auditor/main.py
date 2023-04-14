5c1f8ba0f182dc9cc2d7870eeb4ea42edffb96cb
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2023-04-14 23:25:17 +0100

    NOT DOOM: First refactor for dooms day

--- auditor/main.py
+++ auditor/main.py
@@ -1,26 +1,104 @@
 import pydash as _
 
+from api_comms.github_wrapper import GHWrapper
 from workflow import WorkflowParser, WorkflowVulnAudit
 from auditor.action_auditor import action_audit
 from lib.logger import AuditLogger
+from lib.reporter import report, write_to_file
 from auditor.checkers import (
     get_dangerous_triggers,
     is_workflow_valid,
     check_rce_vuln,
     check_pwn_requests,
-    get_deprecated_commands
+    get_deprecated_commands,
+    get_workflow_actions
 )
+
 
 vuln_analyzer = WorkflowVulnAudit()
 
+def start_scan(target_type, target_input, target_branch, IS_DOOM):
+    gh = GHWrapper()
+    vuln_count = 0
 
-def workflow_analyzer(content: str) -> dict[str, list]:
+    if target_type == "repo":
+        repos = gh.get_single_repo(repo_name=target_input, branch_name=target_branch)
+    else:
+        count, repos = gh.get_multiple_repos(
+            target_name=target_input, branch_name=target_branch, target_type=target_type
+        )
+        AuditLogger.info(f"Metric: Scanning total {count} repos")
+
+    for repo_name in repos:
+        AuditLogger.warning(
+            f"\n\n## Audit: [{repo_name}](https://github.com/{repo_name})"
+        )
+        repo_workflows = repos[repo_name]
+        analysis = repo_analysis(repo_workflows, IS_DOOM)
+
+        for workflow in analysis:
+            vuln_count += len(analysis[workflow]["issues"])
+
+        report(analysis, vuln_count) if vuln_count > 0 else write_to_file("No issues")
+    return vuln_count
+
+
+def repo_analysis(repo_workflow, IS_DOOM: bool):
+    result = {}
+
+    for workflow in repo_workflow:
+        workflow_name = workflow["name"]
+
+        vuln_check = workflow_analyzer(workflow, IS_DOOM)
+        result[workflow_name] = {
+            "secrets": vuln_check["secrets"],
+            "issues": vuln_check["issues"],
+        }
+    return result
+
+def workflow_analyzer(content: str, IS_DOOM:bool) -> dict[str, list]:
+    wrkfl = WorkflowParser(content)
+
+    result, job_elements = breakdown_jobs(wrkfl)
+
+    if IS_DOOM:
+        actions = get_workflow_actions(job_elements)
+        for action in actions:
+            action = action.split("@")
+            target_type = "repo"
+            target_input = action[0]
+            target_branch = "HEAD"
+            IS_DOOM = False
+            start_scan(target_type, target_input, target_branch, IS_DOOM)
+
+    dangerous_triggers = get_dangerous_triggers(triggers=wrkfl.triggers)
+
+    try:
+        deprecated_commands = get_deprecated_commands(wrkfl,job_elements)
+        rce = check_rce_vuln(job_elements)
+        pwn = check_pwn_requests(dangerous_triggers, job_elements)
+        vulnerable_supply_chain = action_audit(job_elements)
+        _.get(result, "issues").append(rce)
+        _.get(result, "issues").append(pwn)
+        _.get(result, "issues").append(vulnerable_supply_chain)
+        _.get(result, "issues").append(deprecated_commands)
+
+    except Exception as workflow_err:
+        AuditLogger.error(
+            f">>> Error parsing workflow. Error is {str(workflow_err)}"
+        )
+
+    result["issues"] = _.flatten_deep(_.get(result, "issues"))
+    result["secrets"] = _.flatten_deep(_.get(result, "secrets"))
+
+    return result
+
+
+def breakdown_jobs(wrkfl):
     result = {
         "issues": [],
         "secrets": [],
     }
-
-    wrkfl = WorkflowParser(content)
 
     if is_workflow_valid(wrkfl):
         result["secrets"].append(wrkfl.secrets)
@@ -29,27 +107,7 @@ def workflow_analyzer(content: str) -> dict[str, list]:
 
         if all_jobs:
             job_elements = get_job_elements_with_id(wrkfl, all_jobs)
-            dangerous_triggers = get_dangerous_triggers(triggers=wrkfl.triggers)
-
-            try:
-                deprecated_commands = get_deprecated_commands(wrkfl,job_elements)
-                rce = check_rce_vuln(job_elements)
-                pwn = check_pwn_requests(dangerous_triggers, job_elements)
-                vulnerable_supply_chain = action_audit(job_elements)
-
-                _.get(result, "issues").append(rce)
-                _.get(result, "issues").append(pwn)
-                _.get(result, "issues").append(vulnerable_supply_chain)
-                _.get(result, "issues").append(deprecated_commands)
-
-            except Exception as workflow_err:
-                AuditLogger.error(
-                    f">>> Error parsing workflow. Error is {str(workflow_err)}"
-                )
-    result["issues"] = _.flatten_deep(_.get(result, "issues"))
-    result["secrets"] = _.flatten_deep(_.get(result, "secrets"))
-
-    return result
+    return (result, job_elements)
 
 
 def get_job_elements_with_id(
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from api_comms.github_wrapper import GHWrapper
 from lib.logger import AuditLogger
 from lib.reporter import report, write_to_file
-from auditor.main import workflow_analyzer
+from auditor.main import workflow_analyzer, start_scan
 
 """
 Input:
@@ -16,52 +16,15 @@ Summary:
     for any potential vulnerabilities. 
 """
 
-
-def repo_analysis(repo_workflow):
-    result = {}
-
-    for workflow in repo_workflow:
-        workflow_name = workflow["name"]
-
-        vuln_check = workflow_analyzer(workflow)
-        result[workflow_name] = {
-            "secrets": vuln_check["secrets"],
-            "issues": vuln_check["issues"],
-        }
-    return result
-
-
 def main():
-    gh = GHWrapper()
-    vuln_count = 0
 
     target_type = os.environ.get("TARGET_TYPE", None)  # repo, org, or user
     target_input = os.environ.get("TARGET_INPUT", None)
     target_branch = os.environ.get("BRANCH", None) or "HEAD"
+    IS_DOOM = os.environ.get("IS_DOOM_DAY", False)
 
-    if target_type == "repo":
-        repos = gh.get_single_repo(repo_name=target_input, branch_name=target_branch)
-    else:
-        count, repos = gh.get_multiple_repos(
-            target_name=target_input, branch_name=target_branch, target_type=target_type
-        )
-        AuditLogger.info(f"Metric: Scanning total {count} repos")
+    result = start_scan(target_type, target_input, target_branch, IS_DOOM)
 
-    for repo_name in repos:
-        AuditLogger.warning(
-            f"\n\n## Audit: [{repo_name}](https://github.com/{repo_name})"
-        )
-        repo_workflows = repos[repo_name]
-        analysis = repo_analysis(repo_workflows)
-
-        for workflow in analysis:
-            vuln_count += len(analysis[workflow]["issues"])
-
-        report(analysis, vuln_count) if vuln_count > 0 else write_to_file("No issues")
-
-    # DO NOT REMOVE PRINT
-    # Used to push result to stdout in the github runner and evaluate if CI should break or not
-    print(vuln_count)
-
+    print(result)
 
 main()
